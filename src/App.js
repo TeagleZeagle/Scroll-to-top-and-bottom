61312b7d870fd369141488b5fa5b76ca56d53487
Kaan Kani İnce <kkince@newmind.ai>
2024-07-10 11:31:11 +0300

    scroll to top and bottom created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ScrollToTopAndBottom from "./components/scroll-to-top-and-bottom";
+
+function App() {
+    return (
+        <div className="App">
+            {/* Scroll to Top and Bottom */}
+            <ScrollToTopAndBottom/>
+        </div>
+    );
+}
+
+export default App;
